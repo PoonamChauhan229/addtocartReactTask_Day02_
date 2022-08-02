b6f9312b9cfbe53d0d69ca63ecb82d062e25f6c9
Poonam Chauhan <68181154+PoonamChauhan229@users.noreply.github.com>
2022-08-02 17:55:04 +0530

    Add files via upload

---
+++ addtocart/src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import DisplayCard from './components/DisplayCard';
+import Footer from './components/Footer';
+import Header from './components/Header';
+import Navbar from './components/Navbar';
+
+function App() {
+  return (
+    <>
+    <Navbar/>
+    <Header/>
+    <DisplayCard/>
+    <Footer/>
+    </>
+  );
+}
+
+export default App;
